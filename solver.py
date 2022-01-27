df97afc1e7cd1586e552624b3bdbf29ae1a1212c
James Addison <jay@jp-hosting.net>
2022-01-27 09:22:52 +0000

    Initial solver implementation

---
+++ solver.py
@@ -0,0 +1,40 @@
+from string import ascii_lowercase
+
+dictionary = open("/usr/share/dict/words", "r")
+words = set()
+vowels = {"a", "e", "i", "o", "u", "y"}
+
+# Collect alphabetical words of length five containing no more than one vowel
+for word in dictionary.readlines():
+    word = word.strip().lower()
+    if len(word) != 5:
+        continue
+    if any(letter not in ascii_lowercase for letter in word):
+        continue
+    if len(vowels & set(word)) > 1:
+        continue
+    words.add(word)
+
+
+def search(letters_remaining, solutions, candidate_words=set()):
+    # Goal case:
+    # - We have found six five-letter words without overlapping letters
+    # - We have consumed all of the letters available
+    if len(candidate_words) == 6 and len(letters_remaining) == 0:
+        solution = ",".join(sorted(candidate_words))
+        solutions.add(solution)
+        return
+
+    # Recursive case: find words without overlapping letters and search those
+    for word in words:
+        if any(letter not in letters_remaining for letter in word):
+            continue
+        reduced_letters = letters_remaining.copy()
+        for letter in word:
+            reduced_letters.discard(letter)
+        search(reduced_letters, solutions, candidate_words | {word})
+
+
+solutions = set()
+search(set(ascii_lowercase), solutions)
+print(solutions)
